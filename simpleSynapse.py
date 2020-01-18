4c0c86c4558847614f0d2bde11a4cf34b9aa1524
apblair <jupyter-user@saturn-f62b321b-0138-4237-8c56-e13d6733cdab-m.c.gladstone-bruneau.internal>
2020-01-18 19:38:21 +0000

    adding setup

--- simpleSynapse.py
+++ simpleSynapse.py
@@ -1,3 +1,11 @@
+#!/usr/bin/python
+
+"""
+Program: simpleSynapse
+Author: Andrew Blair and Robert Hu
+Description: A simple Synapse organization and maintenance python client script.
+"""
+
 import os
 import shutil
 import hashlib
@@ -5,9 +13,9 @@ import synapseclient
 import synapseutils
 from synapseclient import Project, Folder, File, Link
 
-class SimpleSynapse(object):
+class simpleSynapse:
     """
-    A class used to organize and sync a Synapse project. 
+    A simple class used to organize and sync a Synapse project. 
     
     Attributes
     ----------
@@ -20,18 +28,6 @@ class SimpleSynapse(object):
     
     Methods
     -------
-    create_synapse_dict()
-        Create a dictionary for Synapse folders and their ID's
-        
-    file_search(project_name, synapse_dict)
-        Search through a Synapse project directory tree to collect new file metadata and 
-        export the data to (a manifest tsv for a Synapse sync
-        
-    check_sub_folders(folder, synapse_dict)
-        Check if a Synapse folder has sub folders
-        
-    make_synapse_dirs(dir_list)
-        Create a project directory tree and syncs to Synapse
         
     sync_synapse_to_local()
         Sync Synapse to local
@@ -70,9 +66,77 @@ class SimpleSynapse(object):
         '''
         return self.project
     
+    def check_subdirs(self, synapse_folder_dict, paths, subdirs):
+        '''
+        Check if subdirectories are on Synapse, if not sync the folder
+        
+        :param synapse_folder_dict: list, a list of strings that are subdirectories
+        :param paths: str,
+        :param subdirs: list,
+        return synapse_folder_dict: dict,
+        '''
+        for dirs in subdirs:
+            if dirs == '.ipynb_checkpoints':
+                shutil.rmtree(paths + '/' + dirs)
+            else:
+                dir_entityID = self.syn.findEntityId(dirs, parent=synapse_folder_dict[paths.split('/')[-1]])
+                if dir_entityID is not None:
+                    synapse_folder_dict[dirs] = dir_entityID
+                else:
+                    folder = Folder(dirs, parent=synapse_folder_dict[paths.split('/')[-1]])
+                    folder = self.syn.store(folder)
+                    synapse_folder_dict[folder.name] = folder.id
+        return synapse_folder_dict
+    
+    def check_files(self, synapse_folder_dict, files):
+        '''
+        Check if the md5 has changed on Synapse, if not  
+        
+        :param synapse_folder_dict:
+        :param files:
+        return: None
+        '''
+        for data in files:
+            if data == 'SYNAPSE_METADATA_MANIFEST.tsv':
+                os.unlink(paths + '/' + data)
+            else:
+                file_entityID = self.syn.findEntityId(data, parent=synapse_folder_dict[paths.split('/')[-1]])
+                if file_entityID is not None:
+                    file_entity = self.syn.get(file_entityID, downloadFile=False)
+                    local_md5 = synapseclient.utils.md5_for_file(paths + '/' + data).hexdigest()
+                    if file_entity.md5 != local_md5:
+                        self.file_store(paths + '/' + data, synapse_folder_dict[paths.split('/')[-1]])
+                else:
+                    self.file_store(paths + '/' + data, synapse_folder_dict[paths.split('/')[-1]])
+                    
+    def file_store(self, file_path, parent_entityID):
+        '''
+        Create a new Entity or updates an existing Entity, uploading any files in the process to Synapse project
+        
+        :param file_path: str, path to the file that will be uploaded to Synapse
+        :param parent_entityID: str, An id or an object of a Synapse entity that is the parent of the file being uploaded
+        return: None
+        '''
+        file = File(file_path, parent=parent_entityID)
+        file = self.syn.store(file)
+        
+    def sync_local_to_synapse(self):
+        '''
+        Synac local to Synapse, only if the md5 is not the same
+        
+        :param: None
+        return: None
+        '''
+        synapse_folder_dict = {self.project.name :self.project.id}
+        for paths, subdirs, files in os.walk(self.project.name):
+            if subdirs:
+                synapse_folder_dict = self.check_subdirs(synapse_folder_dict, paths, subdirs)
+            if files:
+                self.check_files(synapse_folder_dict, paths, files)
+    
     def sync_synapse_to_local(self):
         '''
-        Sync Synapse to local
+        Sync Synapse to local, without overwriting local entities
         
         :param: None
         return: None
@@ -80,37 +144,3 @@ class SimpleSynapse(object):
         if not os.path.exists(self.project['name']): 
             os.makedirs(self.project['name'])
         synapseutils.sync.syncFromSynapse(self.syn, self.project['id'], path=self.project['name'], ifcollision="keep.local")
-    
-    def file_store(self, file_path, parent_path):
-        file = File(file_path, parent=parent_path)
-        file = self.syn.store(file)
-    
-    def check(self):
-        synapse_folder_dict = {self.project.name :self.project.id}
-        synapse_file_dict = {}
-        for paths, subdirs, files in os.walk(self.project.name):
-            if subdirs:
-                for dirs in subdirs:
-                    if dirs == '.ipynb_checkpoints':
-                        shutil.rmtree(paths + '/' + dirs)
-                    else:
-                        dir_entityID = self.syn.findEntityId(dirs, parent=synapse_folder_dict[paths.split('/')[-1]])
-                        if dir_entityID is not None:
-                            synapse_folder_dict[dirs] = dir_entityID
-                        else:
-                            folder = Folder(dirs, parent=synapse_folder_dict[paths.split('/')[-1]])
-                            folder = self.syn.store(folder)
-                            synapse_folder_dict[folder.name] = folder.id
-            if files:
-                for data in files:
-                    if data == 'SYNAPSE_METADATA_MANIFEST.tsv':
-                        os.unlink(paths + '/' + data)
-                    else:
-                        file_entityID = self.syn.findEntityId(data, parent=synapse_folder_dict[paths.split('/')[-1]])
-                        if file_entityID is not None:
-                            file_entity = self.syn.get(file_entityID, downloadFile=False)
-                            local_md5 = synapseclient.utils.md5_for_file(paths + '/' + data).hexdigest()
-                            if file_entity.md5 != local_md5:
-                                self.file_store(paths + '/' + data, synapse_folder_dict[paths.split('/')[-1]])
-                        else:
-                            self.file_store(paths + '/' + data, synapse_folder_dict[paths.split('/')[-1]])
