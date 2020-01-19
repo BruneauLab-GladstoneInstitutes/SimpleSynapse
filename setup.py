f96af73267a9cbf5bc3d16b69bab34c08bf85d3e
System Administrator <root@LAXUCD07855.local>
2020-01-18 18:21:35 -0800

    pip version 0.3, next update will include api calls and cli

--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
 from setuptools import setup
 
 setup(name='simpleSynapse',
-      version='0.1',
+      version='0.3',
       description='A simple Synapse organization and maintenance python client script',
       url='https://github.com/BruneauLab-GladstoneInstitutes/simpleSynapse',
       author='Andrew Blair',
       author_email='andrew.blair@gladstone.ucsf.edu',
       license='MIT',
-      scripts=['simpleSynapse.py'],
+      packages=['simpleSynapse'],
       install_requires=[
           'synapseclient',
       ],
